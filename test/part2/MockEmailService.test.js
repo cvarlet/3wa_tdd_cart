76932f5b8ee3f13f13c0daa1ab85e66ae3b0c464
cvarlet <varlet.charlotte@gmail.com>
2025-09-24 14:46:24 +0200

    order snapshot

--- test/part2/MockEmailService.test.js
+++ test/part2/MockEmailService.test.js
@@ -1,6 +1,6 @@
 const { MockEmailService } = require("./doubles/MockEmailService");
 
-describe.skip("MockEmailService", () => {
+describe("MockEmailService", () => {
   let mockEmailService;
   const order = {
     email: "test@example.com",
@@ -16,5 +16,9 @@ describe.skip("MockEmailService", () => {
   it("Correct scenario : Should send a confirmation email", () => {
     mockEmailService.sendCheckoutConfirmationEmail(order);
     expect(mockEmailService.getSentEmails().length).toBe(1);
+  });
+
+  it("Correct scenario : should match order snapshot", () => {
+    expect(order).toMatchSnapshot();
   });
 });
